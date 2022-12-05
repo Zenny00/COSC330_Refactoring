1fe0ada904e1b95dee41aa1de74d8e0cbb5568f3
jcomfort1 <joshuacomfort@gmail.com>
2022-12-05 13:02:18 -0500

    Added 12 JUnit tests.

--- RefactoringTest.java
+++ RefactoringTest.java
@@ -44,7 +44,92 @@ public class RefactoringTest
 		assertThat(customer_1.statement(), is(expectedMessageFor("TEST TITLE 3", 3.0, 3.0, 1)));
 	}
 
+	@Test
+	public void basicNewReleaseRentalTest()
+	{
+		//Add new rental
+		customer_1.addRental(new Rental(movie_2, 1));
+		//Check if the new rental was correctly added
+		assertThat(customer_1.statement(), is(expectedMessageFor("TEST TITLE 2", 3.0, 3.0, 1)));
+	}
 
+	@Test
+	public void noDiscountNewReleaseRentalTestButShouldGetFrequentRenterPoints()
+	{
+		//Add new rental, should have frequent renter points
+		customer_1.addRental(new Rental(movie_2, 4));
+		//Check if the new rental was correctly added
+		assertThat(customer_1.statement(), is(expectedMessageFor("TEST TITLE 2", 12.0, 12.0, 2)));
+	}
+
+	@Test
+	public void addBasicRegularRentalTest()
+	{
+		//Add new regular rental, should have no discounts or frequent renter points
+		customer_1.addRental(new Rental(movie_1, 2));
+		//Check if the new rental was correctly added
+		assertThat(customer_1.statement(), is(expectedMessageFor("TEST TITLE 1", 2.0, 2.0, 1)));
+	}
+
+	@Test
+	public void addDiscountedRegularRental()
+	{
+		//Add new regular rental, should produce a discount
+		customer_1.addRental(new Rental(movie_1, 4));
+		//Check if the new rental was correctly added, if so it should give a discount
+		assertThat(customer_1.statement(), is(expectedMessageFor("TEST TITLE 1", 5.0, 5.0, 1)));
+	}
+
+	@Test
+	public void sumVariousRentalsTest()
+	{
+		//Add a bunch of new rentals
+		customer_1.addRental(new Rental(movie_1, 1));
+		customer_1.addRental(new Rental(movie_3, 2));
+		customer_1.addRental(new Rental(movie_2, 3));
+
+		assertThat(customer_1.statement(), is("Rental record for Franklin\n\tTEST TITLE 1\t2.0\n\tTEST TITLE 3\t1.5\n\tTEST TITLE 2\t9.0\nAmount owed is 12.5\nYou earned 4 frequent renter points"));
+	}
+
+	@Test
+	public void createNewRentalGetMovie()
+	{
+		//Create new rental using movie_1
+		Rental rental = new Rental(movie_1, 1);
+		//Check if the correct movie is returned
+		assertThat(rental.getMovie().getTitle(), is(movie_1.getTitle()));
+	}
+
+	@Test
+	public void createNewRentalGetDaysRented()
+	{
+		//Create a new rental that will be rented for 8 days
+		Rental rental = new Rental(movie_2, 8);
+		//Check if the movie was rented for 8 days
+		assertThat(rental.getDaysRented(), is(8));
+	}
+
+	@Test
+	public void createNewMovieGetPriceCode()
+	{
+		//Create a new REGULAR movie
+		Movie new_regular_movie = new Movie("TEST TITLE 4", Movie.REGULAR);
+		//Check if the movie was created properly
+		assertThat(new_regular_movie.getPriceCode(), is(Movie.REGULAR));
+	}
+
+	@Test
+	public void createNewMovieSetPriceCode()
+	{
+		//Create a new REGULAR movie
+		Movie new_regular_movie = new Movie("TEST TITLE 4", Movie.REGULAR);
+		//Change the movie to a new release
+		new_regular_movie.setPriceCode(Movie.NEW_RELEASE);
+		//Check if the movie price code was changed
+		assertThat(new_regular_movie.getPriceCode(), is(Movie.NEW_RELEASE));
+	}
+
+	//Helper function to create an expected output String for the customer statement
 	private static String expectedMessageFor(String rental, double price, double total, int renterPointsEarned) {
         return "Rental record for Franklin\n\t" + rental + "\t" + price + "\nAmount owed is " + total + "\nYou earned " + renterPointsEarned + " frequent renter points";
     	}
