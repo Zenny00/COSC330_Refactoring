9b388f4bbdbf27fcc2f091322da6835030bb977a
jcomfort1 <joshuacomfort@gmail.com>
2022-12-05 14:46:02 -0500

    Moved the getAmount method from the Customer class to the Rental class. Passed all 12 tests

--- Customer.java
+++ Customer.java
@@ -23,27 +23,9 @@ public class Customer {
     //We make this method private as it will never be called outside of the statement method
     //Using extract method we are moving the switch statement to a separate method
     private double getAmount(Rental current_rental)
-    {	
-	    double amount_charged = 0; //Local variable to hold the amount from each movie
-            
-	    //Switch statement to find the correct amount for each movie using the PriceCode
-	    switch (current_rental.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    amount_charged += 2;
-                    if (current_rental.getDaysRented() > 2)
-                        amount_charged += (current_rental.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    amount_charged += current_rental.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDREN:
-                    amount_charged += 1.5;
-                    if (current_rental.getDaysRented() > 3)
-                        amount_charged += (current_rental.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-
-	    return amount_charged; //Return the amount found
+    {
+    	//Delegation, have the current rental return the amount charged
+	return current_rental.getAmountCharged();	    
     }
 
     //This method is too long, lets move the switch statement to a separate function
--- Rental.java
+++ Rental.java
@@ -21,4 +21,28 @@ public class Rental {
         return daysRented;
     }
 
+    //Get the amount 
+    public double getAmountCharged()
+    {
+	    double amount_charged = 0; //Local variable to hold the amount from each movie
+            
+	    //Switch statement to find the correct amount for each movie using the PriceCode
+	    switch (getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    amount_charged += 2;
+                    if (getDaysRented() > 2)
+                        amount_charged += (getDaysRented() - 2) * 1.5;
+                    break;
+                case Movie.NEW_RELEASE:
+                    amount_charged += getDaysRented() * 3;
+                    break;
+                case Movie.CHILDREN:
+                    amount_charged += 1.5;
+                    if (getDaysRented() > 3)
+                        amount_charged += (getDaysRented() - 3) * 1.5;
+                    break;
+            }
+
+	    return amount_charged; //Return the amount found
+    }
 }
