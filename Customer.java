8ee80c71ed48f5f95a15ac4a51cd210fd3a14148
jcomfort1 <joshuacomfort@gmail.com>
2022-12-05 13:49:59 -0500

    Renamed variables

--- Customer.java
+++ Customer.java
@@ -22,54 +22,56 @@ public class Customer {
 
     //We make this method private as it will never be called outside of the statement method
     //Using extract method we are moving the switch statement to a separate method
-    private double getAmount(Rental rental)
+    private double getAmount(Rental current_rental)
     {	
-	    double amount = 0; //Local variable to hold the amount from each movie
+	    double amount_charged = 0; //Local variable to hold the amount from each movie
             
 	    //Switch statement to find the correct amount for each movie using the PriceCode
-	    switch (rental.getMovie().getPriceCode()) {
+	    switch (current_rental.getMovie().getPriceCode()) {
                 case Movie.REGULAR:
-                    amount += 2;
-                    if (rental.getDaysRented() > 2)
-                        amount += (rental.getDaysRented() - 2) * 1.5;
+                    amount_charged += 2;
+                    if (current_rental.getDaysRented() > 2)
+                        amount_charged += (current_rental.getDaysRented() - 2) * 1.5;
                     break;
                 case Movie.NEW_RELEASE:
-                    amount += rental.getDaysRented() * 3;
+                    amount_charged += current_rental.getDaysRented() * 3;
                     break;
                 case Movie.CHILDREN:
-                    amount += 1.5;
-                    if (rental.getDaysRented() > 3)
-                        amount += (rental.getDaysRented() - 3) * 1.5;
+                    amount_charged += 1.5;
+                    if (current_rental.getDaysRented() > 3)
+                        amount_charged += (current_rental.getDaysRented() - 3) * 1.5;
                     break;
             }
 
-	    return amount; //Return the amount found
+	    return amount_charged; //Return the amount found
     }
 
     //This method is too long, lets move the switch statement to a separate function
     public String statement() {
-        int frequentRenterPoints = 0;
+	int num_frequent_renter_points = 0;
+	int statement_amount = 0;
 
-        String result = "Rental record for " + getName() + "\n";
-        for (Rental rental : rentals) { 
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
-                frequentRenterPoints++;
+        String statement_result = "Rental record for " + getName() + "\n";
+        for (Rental rental : rentals) {
+		//Local variable to hold the value of the amount on each rental
+		double rental_amount = getAmount(rental);
 
-	    //Local variable to hold the value of the amount on each rental
-	    double amount = getAmount(rental);
+           	// add frequent renter points
+            	num_frequent_renter_points++;
 
-            // show figures for this rental
-            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(amount) + "\n";
+		// add bonus for a two day new release rental
+		if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
+			num_frequent_renter_points++;
 
-            totalAmount += amount;
+		// show figures for this rental
+		statement_result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental_amount) + "\n";
+
+		statement_amount += rental_amount;
         }
 
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        statement_result += "Amount owed is " + String.valueOf(statement_amount) + "\n";
+        statement_result += "You earned " + String.valueOf(num_frequent_renter_points) + " frequent renter points";
 
-        return result;
+        return statement_result;
     }
 }
