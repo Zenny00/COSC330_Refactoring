ed55419e89d6bdf720777ef40439edbdfa5eca81
jcomfort1 <joshuacomfort@gmail.com>
2022-12-07 10:16:45 -0500

    Replaced conditional logic with polymorphism, now uses delegation. Passed all 13 tests.

--- Movie.java
+++ Movie.java
@@ -25,5 +25,28 @@ public class Movie {
     public void setPriceCode(int priceCode) {
         this.priceCode = priceCode;
     }
+   
+    //Get the amount 
+    public double getAmountCharged(int days_rented)
+    {
+	    double amount_charged = 0; //Local variable to hold the amount from each movie
+	    //Switch statement to find the correct amount for each movie using the PriceCode
+	    switch (getPriceCode()) {
+                case Movie.REGULAR:
+                    amount_charged += 2;
+                    if (days_rented > 2)
+                        amount_charged += (days_rented - 2) * 1.5;
+                    break;
+                case Movie.NEW_RELEASE:
+                    amount_charged += days_rented * 3;
+                    break;
+                case Movie.CHILDREN:
+                    amount_charged += 1.5;
+                    if (days_rented > 3)
+                        amount_charged += (days_rented - 3) * 1.5;
+                    break;
+            }
 
+	    return amount_charged; //Return the amount found            
+    }
 }
--- Rental.java
+++ Rental.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Rental {
 
-    private Movie movie;
+    Movie movie;
     private int daysRented;
 
     public Rental(Movie movie, int daysRented) {
@@ -21,29 +21,10 @@ public class Rental {
         return daysRented;
     }
 
-    //Get the amount 
+    //Get the amount, use delegation
     public double getAmountCharged()
-    {
-	    double amount_charged = 0; //Local variable to hold the amount from each movie
-            
-	    //Switch statement to find the correct amount for each movie using the PriceCode
-	    switch (getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    amount_charged += 2;
-                    if (getDaysRented() > 2)
-                        amount_charged += (getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    amount_charged += getDaysRented() * 3;
-                    break;
-                case Movie.CHILDREN:
-                    amount_charged += 1.5;
-                    if (getDaysRented() > 3)
-                        amount_charged += (getDaysRented() - 3) * 1.5;
-                    break;
-            }
-
-	    return amount_charged; //Return the amount found
+    { 
+	    return movie.getAmountCharged(daysRented); //Return the amount found            
     }
 
     //Move method to get the number of frequent renter points
