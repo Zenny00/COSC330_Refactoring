1ecd994c0d9fd20ba2cd5c814e97019d9329668f
jcomfort1 <joshuacomfort@gmail.com>
2022-12-07 08:48:32 -0500

    Removed temp variable totalAmount from the statement method in the Customer class.

--- Customer.java
+++ Customer.java
@@ -23,7 +23,6 @@ public class Customer {
     //This method is too long, lets move the switch statement to a separate function
     public String statement() {
 	int num_frequent_renter_points = 0;
-	int statement_amount = 0;
 
         String statement_result = "Rental record for " + getName() + "\n";
         for (Rental rental : rentals) {
@@ -33,12 +32,23 @@ public class Customer {
 		// show figures for this rental
 		statement_result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getAmountCharged()) + "\n";
 
-		statement_amount += rental.getAmountCharged();
         }
 
-        statement_result += "Amount owed is " + String.valueOf(statement_amount) + "\n";
+	//Use a function call rather than a temp variable
+        statement_result += "Amount owed is " + String.valueOf(getTotalAmountCharged()) + "\n";
         statement_result += "You earned " + String.valueOf(num_frequent_renter_points) + " frequent renter points";
 
         return statement_result;
+    }
+
+    //Calculate the total amount to be charged
+    public double getTotalAmountCharged()
+    {
+	double totalAmountCharged = 0;
+	//Loop through each rental and add its charge to the total
+        for (Rental rental : rentals)
+    		totalAmountCharged += rental.getAmountCharged(); //Add the amount charged for the current rental
+    
+    	return totalAmountCharged; //Return the total
     }
 }
