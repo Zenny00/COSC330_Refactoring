4906158edd183acb85cea65ac50b96ce8b2a9801
jcomfort1 <joshuacomfort@gmail.com>
2022-12-07 08:52:50 -0500

    Removed local variable numFrequentRenterPoints and replaced it with a method call to calculate the total number of frequent renter points, all 12 tests passed.

--- Customer.java
+++ Customer.java
@@ -22,21 +22,15 @@ public class Customer {
 
     //This method is too long, lets move the switch statement to a separate function
     public String statement() {
-	int num_frequent_renter_points = 0;
-
         String statement_result = "Rental record for " + getName() + "\n";
         for (Rental rental : rentals) {
-		//Calculate the number of frequent renter points
-		num_frequent_renter_points += rental.getFrequentRenterPoints();
-
 		// show figures for this rental
 		statement_result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getAmountCharged()) + "\n";
-
         }
 
 	//Use a function call rather than a temp variable
         statement_result += "Amount owed is " + String.valueOf(getTotalAmountCharged()) + "\n";
-        statement_result += "You earned " + String.valueOf(num_frequent_renter_points) + " frequent renter points";
+        statement_result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
 
         return statement_result;
     }
@@ -50,5 +44,20 @@ public class Customer {
     		totalAmountCharged += rental.getAmountCharged(); //Add the amount charged for the current rental
     
     	return totalAmountCharged; //Return the total
+    }
+
+    //Similar to the above method, we remove the local num_frequent_renter_points temp variable and have a method return the total
+    public int getTotalFrequentRenterPoints()
+    {
+	//Initialize number of points to 0
+	int num_frequent_renter_points = 0;
+    	
+	//Iterate through all the rentals and add the number of frequent renter points
+        for (Rental rental : rentals)
+		//Calculate the number of frequent renter points
+		num_frequent_renter_points += rental.getFrequentRenterPoints();
+    
+	//Return the total number of frequent renter points
+	return num_frequent_renter_points;
     }
 }
