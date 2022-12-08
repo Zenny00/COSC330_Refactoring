1d15745451263f334c6815ab5c78a328c50df674
Justin <jconklin329@gmail.com>
2022-12-08 18:55:00 -0500

    more bug fixes, priceCode needed to be renamed price, getCharge wasn't proper function name in Price.java

--- Price.java
+++ Price.java
@@ -2,7 +2,7 @@ package bad.robot.refactoring.chapter1;
 
 public abstract class Price {
     abstract int getPriceCode();
-    abstract double getCharge(int daysRented);
+    abstract double getAmountCharged(int daysRented);
 
     public int getFrequentRenterPoints(int daysRented) {
         return 1;
